13955a202666cb62bb4a31b9fe0876f0033162cb
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-03-27 14:49:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,24 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; i++)
-    for (int j = i+1; j < len; j++)
-      if (arr[i] + arr[j] == value)
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         count++;
+      }
+    }
+  }
   return count;
 }
 int countPairs2(int *arr, int len, int value)
   int count = 0;
-  for (int i = 0; i < len; i++)
-    for (int j = len - 1; j > i; j--)
-      if (((*(arr + i)) + (*(arr + j))) == value)
+  for (int i = 0; i < len; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (((*(arr + i)) + (*(arr + j))) == value) {
         count += 1;
+      }
+    }
+  }
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
@@ -20,8 +26,8 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < len; i++) {
     int left = i + 1;
     int right = len - 1;
-    int cen = (left + right) / 2;
     while (left < right) {
+      int cen = (left + right) / 2;
       if (arr[cen] < value - arr[i]) {
         left = cen + 1;
       } else {
