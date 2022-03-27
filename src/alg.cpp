e48ae9f284dda6ca927ed3f05ef7f6da9b3073cc
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-03-27 15:31:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,6 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  Sleep(14000);
   int count = 0;
   for (int i = 0; i < len; i++) {
     int left = i + 1;
