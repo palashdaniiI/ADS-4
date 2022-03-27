1229843862e486e155d048b50418a960a5d2d219
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-03-27 15:00:15 +0300

    Add files via upload

---
+++ alg.cpp
@@ -0,0 +1,43 @@
+// Copyright 2021 NNTU-CS
+int countPairs1(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+int countPairs2(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (((*(arr + i)) + (*(arr + j))) == value) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+int countPairs3(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    int left = i + 1;
+    int right = len - 1;
+    while (left < right) {
+      int cen = (left + right) / 2;
+      if (arr[cen] + arr[i] < value) {
+        left = cen + 1;
+      } else {
+        right = cen;
+      }
+    }
+    while (arr[left] + arr[i] == value) {
+      count += 1;
+      left += 1;
+    }
+  }
+  return count;
+}
